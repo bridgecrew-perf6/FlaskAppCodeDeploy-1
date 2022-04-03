eaf032a1ae790cdf7247185dae8d54f0435129a8
Ravi Tandon <searchatwatson@gmail.com>
2022-04-03 15:07:48 -0400

    Minor Change.

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
-
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
-@app.route('/ping')
 @app.route('/index')
 def index():
     return render_template('index.html')
